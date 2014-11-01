79a99ff3a5a725847f72e6478e572a6b5de167dd
Clay Smith <smithclay@gmail.com>
2014-11-01 13:26:13 -0700

    update for twitter auth

--- undisclosed/Controllers/AppDelegate.swift
+++ undisclosed/Controllers/AppDelegate.swift
@@ -10,10 +10,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, nil)
 
-        PFTwitterUtils.initializeWithConsumerKey("ANURquEXPaR78bRnVSEHusqsh", consumerSecret: "u84sUYXa9QsH05Lmqrul1sK4OZrniycMOn0Rtmk1kswCaICSZX")
+        PFTwitterUtils.initializeWithConsumerKey("rqP21Psqq6aow4bzwGVJVXjax", consumerSecret: "8cuK8Kdigst4k18giKWnVAumWozWal2R0DUUuW4CHS65rC8AvF")
 
         PFTwitterUtils.logInWithBlock { (user: PFUser!, error: NSError!) -> Void in
-            NSLog(error.localizedDescription)
             if user == nil {
                 NSLog("Uh oh. The user cancelled the Twitter login.")
                 return
